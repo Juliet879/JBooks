b3499cc921e4ea220d1d743b9e977eedcaba7b49
JulietK <julietgisemba000@>
2020-10-22 02:03:51 +0300

    modified files

---
+++ app/src/main/java/com/moringaschool/jbooks/Constants.java
@@ -0,0 +1,6 @@
+package com.moringaschool.jbooks;
+
+public class Constants {
+    public static final  String GOOGLE_API_KEY = BuildConfig.GOOGLE_API_KEY;
+    public static final String GOOGLE_BASE_URL = "https://www.googleapis.com/books/v1/volumes";
+}
