248927ba2c010bfb92139d6c729b89e2b086058c
JulietK <julietgisemba000@>
2020-10-22 02:08:59 +0300

    modified files

--- app/src/main/java/com/moringaschool/jbooks/GoogleApi.java
+++ app/src/main/java/com/moringaschool/jbooks/GoogleApi.java
@@ -1,4 +1,16 @@
 package com.moringaschool.jbooks;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface GoogleApi {
+    @GET("books/")
+    Call<GoogleBookSearchResponse> getBooks(
+            @Query("book") String book,
+            @Query("term") String term,
+            @Query("api_key") String api_key
+    );
+
+
 }
---
+++ app/src/main/java/com/moringaschool/jbooks/GoogleClient.java
@@ -0,0 +1,4 @@
+package com.moringaschool.jbooks;
+
+public class GoogleClient {
+}
