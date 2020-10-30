5dfac7508b53f87d9885f9f1cda84f431db7e2a3
Juliet <julielgisemba000@gmail.com>
2020-10-30 18:55:13 +0300

    modified files

--- app/src/main/java/com/moringaschool/jbooks/ui/BookDetailActivity.java
+++ app/src/main/java/com/moringaschool/jbooks/ui/BookDetailActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.moringaschool.jbooks.R;
+
 public class BookDetailActivity extends AppCompatActivity {
 
     @Override
