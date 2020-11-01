a1b4fc10725137606836b24980ca6199895acff6
Juliet <julielgisemba000@gmail.com>
2020-11-01 16:41:49 +0300

    modified files

--- app/src/main/java/com/moringaschool/jbooks/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/jbooks/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.moringaschool.jbooks.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,10 +8,15 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -34,9 +40,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @BindView(R.id.getStartedButton) Button mGetStartedButton;
     @BindView(R.id.bookEditText) EditText mbookEditText;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //display welcome message
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    getSupportActionBar().setTitle("Welcome, " + user.getDisplayName() + "!");
+                } else {
+
+                }
+
+            }
+        };
 //        mSearchedBookReference = FirebaseDatabase
 //                .getInstance()
 //                .getReference()
@@ -60,11 +87,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        });
 //
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ButterKnife.bind(this);
-
 
 //        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 //        mEditor = mSharedPreferences.edit();
@@ -72,6 +94,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mGetStartedButton.setOnClickListener(this);
         mSavedBookButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
             @Override
             public void onClick(View v) {
@@ -93,6 +130,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 }
             }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
 //    public void saveBookToFirebase(String book) {
 //        mSearchedBookReference.push().setValue(book);
 //    }
