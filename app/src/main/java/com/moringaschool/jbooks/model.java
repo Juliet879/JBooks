041bc0155cbfd98db3bd8f7a17d96d2ec02c941f
Juliet <julielgisemba000@gmail.com>
2020-11-02 00:38:58 +0300

    modified files

---
+++ app/src/main/java/com/moringaschool/jbooks/model.java
@@ -0,0 +1,16 @@
+package com.moringaschool.jbooks;
+
+public class model {
+    private String Book;
+
+    public model(String book) {
+        Book = book;
+    }
+
+    public model() {
+    }
+
+    public String getBook() {
+        return Book;
+    }
+}
